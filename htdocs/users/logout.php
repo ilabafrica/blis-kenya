a541f9e1f3c3569955c55e2e6df5c1877b5ccd05
briankip <kipropbrian@gmail.com>
2013-08-20 16:15:40 +0300

    Accesslog and part of audit trail

--- htdocs/users/logout.php
+++ htdocs/users/logout.php
@@ -1,7 +1,18 @@
 <?php
-session_start();
+include("../includes/db_lib.php");
+
+//Logging User logout
+$auditTrail = new AuditTrail();
+$auditTrail->log_access($_SESSION['user_id'], $auditTrail::LOGOUT, $_SESSION['username']);
+
 session_unset();
 session_destroy();
+
+// Remove and regenerate new session ID
+	session_write_close();
+    setcookie(session_name(),'',0,'/');
+    session_regenerate_id(true);
+	
 if(isset($_REQUEST['timeout']))
 	header("Location:login.php?to");
 else
