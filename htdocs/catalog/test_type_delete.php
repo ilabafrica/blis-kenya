6aa592b37be45049df5e99adbdf251d4c1b3f936
briankip <kipropbrian@gmail.com>
2014-01-25 02:04:59 -0800

    fix for test type delete

--- htdocs/catalog/test_type_delete.php
+++ htdocs/catalog/test_type_delete.php
@@ -7,14 +7,8 @@
 
 include("../includes/db_lib.php");
 
-$saved_session = SessionUtil::save();
-$saved_db = DbUtil::switchToGlobal();
-
 $test_type_id = $_REQUEST['id'];
 TestType::deleteById($test_type_id);
-
-DbUtil::switchRestore($saved_db);
-SessionUtil::restore($saved_session);
 
 header("Location: catalog.php?tdel");
 ?>
