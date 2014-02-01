daa89c98e571df2c00bc1ac4976f2f11bb67d71c
royrutto <royrutto@gmail.com>
2014-02-01 15:38:25 +0300

    push result fix for empy comments with space characters.

--- htdocs/ajax/push_results.php
+++ htdocs/ajax/push_results.php
@@ -29,7 +29,7 @@ foreach ($lab_numbers as $lab_no){
 	$result_ent = $lab_no['result'];
 	$comments = $lab_no['comments'];
 	
-	if($comments==null or $comments==''){
+	if($comments==null or $comments==''  or str_replace(' ', '', $comment)==''){
 		$comments = 'No Comments';
 	}
 	
