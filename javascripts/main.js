4c730d996eb8f9df4525a7bee403eae5349b7b6f
Brian Maiyo <kipropbrian@gmail.com>
2014-07-03 11:04:43 +0300

    Finally looks nice, I did it

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,19 +1,28 @@
 function slideswitch(){	
 	var $activeslide = $('#slideshow img.active');
 		if ($activeslide == 0) $activeslide = $('#slideshow img:last');	
-	var $activeback = $('#background img.active');
-		if ($activeback == 0) $activeback = $('#background img:last');	
+	
 	var $nextslide = $activeslide.next().length ? $activeslide.next() : $('#slideshow img:first') ;
-	var $nextback = $activeback.next().length ? $activeback.next() : $('#background img:first')
-
+	
 	$activeslide.addClass('last-active');
-	$activeback.addClass('last-active');
 
 	$nextslide.css({opacity: 0.0})
 		.addClass('active')
 		.animate({opacity: 1.0}, 1000, function(){
 			$activeslide.removeClass('active last-active');
 	});
+
+	
+}
+
+function backswitch(){
+	
+	var $activeback = $('#background img.active');
+		if ($activeback == 0) $activeback = $('#background img:last');
+
+	var $nextback = $activeback.next().length ? $activeback.next() : $('#background img:first');	
+	
+	$activeback.addClass('last-active');		
 
 	$nextback.css({opacity: 0.0})
 		.addClass('active')
@@ -22,6 +31,12 @@ function slideswitch(){
 	});
 }
 
+function animater(){
+	slideswitch();
+	backswitch();
+}
+
 $(function() {
-	setInterval( "slideswitch()", 3000);
+	setInterval( "animater()", 3000);
+
 });
